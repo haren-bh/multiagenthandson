63c64c4ad4e3f02983abcc3586989da4cf082eeb
haren-bh <bhandari.harendra@gmail.com>
2026-04-28 17:03:30 +0900

    Update config.py
    model updated.

--- image_scoring_adk_a2a_server/remote_a2a/image_scoring/config.py
+++ image_scoring_adk_a2a_server/remote_a2a/image_scoring/config.py
@@ -8,5 +8,5 @@ GCS_BUCKET_NAME = os.environ.get("GCS_BUCKET_NAME") or os.getenv("GCS_BUCKET_NAM
 SCORE_THRESHOLD = int(os.getenv("SCORE_THRESHOLD", 45))
 MAX_ITERATIONS = int(os.getenv("MAX_ITERATIONS", 1))
 IMAGEN_MODEL = os.getenv("IMAGEN_MODEL", "imagen-3.0-generate-002")
-GENAI_MODEL = os.getenv("GENAI_MODEL", "gemini-2.0-flash")
+GENAI_MODEL = os.getenv("GENAI_MODEL", "gemini-2.5-flash")
 
